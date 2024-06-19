3aee0176c9c509c11048137939bdb40bdaf63858
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-06-19 23:26:43 +0700

    Add TM Bedroom scene

--- Test/scenes/ThanhMaiBakery/DoorDetectInside.gd
+++ Test/scenes/ThanhMaiBakery/DoorDetectInside.gd
@@ -1,27 +1,25 @@
 extends Area2D
 
 var entered = false
-@onready var label_door_out = $LabelDoorOut
-@export var label_text: String = "[E] Go out"
+@onready var label_out = $LabelDoorOut
 # Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	label_door_out.text = label_text
-	label_door_out.global_position.y = 737
-	label_door_out.global_position.x = 230
-	label_door_out.hide()  # Ensure the label is initially hidden
+	label_out.global_position.y = 747
+	label_out.global_position.x = 232
+	label_out.hide()  # Ensure the label is initially hidden
 	
 func _on_body_entered(body: CharacterBody2D):
 	entered = true
-	label_door_out.show()
+	label_out.show()
 	print("Body entered, label shown")  # Debug print
 
-func _on_body_exited(body):
+func _on_body_exited(body: CharacterBody2D):
 	entered = false
-	label_door_out.hide()
+	label_out.hide()
 	print("Body exit, label off")  # Debug print
 	
 func _process(delta):  
 	if entered == true:
 		if Input.is_action_just_pressed("opendoor"):
-			get_tree().change_scene_to_file("res://scenes/world.tscn")
+			get_tree().change_scene_to_file("res://scenes/ThanhMaiBakery/TMBedroom.tscn")
---
+++ Test/scenes/ThanhMaiBakery/TMBedroom.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var entered = false
+@onready var label_bedroom = $LabelBedroom
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	label_bedroom.global_position.y = 209
+	label_bedroom.global_position.x = 492
+	label_bedroom.hide()  # Ensure the label is initially hidden
+	
+func _on_body_entered(body: CharacterBody2D):
+	entered = true
+	label_bedroom.show()
+	print("Body entered, label shown")  # Debug print
+
+func _on_body_exited(body: CharacterBody2D):
+	entered = false
+	label_bedroom.hide()
+	print("Body exit, label off")  # Debug print
+	
+func _process(delta):  
+	if entered == true:
+		if Input.is_action_just_pressed("opendoor"):
+			get_tree().change_scene_to_file("res://scenes/ThanhMaiBakery/TMBedroom.tscn")
